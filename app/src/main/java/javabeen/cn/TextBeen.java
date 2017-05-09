009adb06f5739aa3ec188158cdfd5c17e34ae144
YSY <313008602@qq.com>
2017-05-09 10:09:09 +0800

    第三次雨量监测

---
+++ app/src/main/java/javabeen/cn/TextBeen.java
@@ -0,0 +1,8 @@
+package javabeen.cn;
+
+/**
+ * Created by Administrator on 2017/5/9.
+ */
+
+public class TextBeen {
+}
---
+++ app/src/main/java/net/model/TextModel.java
@@ -0,0 +1,8 @@
+package net.model;
+
+/**
+ * Created by Administrator on 2017/5/9.
+ */
+
+public class TextModel {
+}
