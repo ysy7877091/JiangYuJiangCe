e256ca6a192afe1e7a9eb65f0bfd29b983d514f8
YSY <313008602@qq.com>
2017-05-10 17:28:32 +0800

    注册登录页

---
+++ app/src/main/java/mytext/administrator/example/com/jiangyujiangce/JiangYuMap.java
@@ -0,0 +1,11 @@
+package mytext.administrator.example.com.jiangyujiangce;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Administrator on 2017/5/10.
+ */
+
+public class JiangYuMap extends AppCompatActivity {
+
+}
